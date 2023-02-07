51c4b65379de36f5c56673dc69abffdaba121eeb
Jihye19249205 <119666066+Jihye19249205@users.noreply.github.com>
2023-02-07 11:47:57 +0000

    [update]nagano_cake

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,17 @@
+class Public::CustomersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def confirm_withdraw
+  end
+  
+  def withdraw
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,46 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   # 会員ページのルーティング↓
-  
-  
-  # 管理者ページのルーティング↓
-  namespace :admin do
-    
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  scope module: :public do
+    root to: "homes#top"
+    get '/about' => "homes#about", as: 'about'
+    resources :items, only:[:index, :show]
+    resources :customers, only: [:show, :edit, :update] do
+      collection do
+        get 'confirm_withdraw'
+        patch 'withdraw'
+      end
+    end
+    resources :cart_items, only: [:index, :create, :update, :destroy] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post 'confirm_order'
+        put 'complete'
+      end
+    end
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
-  
+  # 管理者ページのルーティング↓
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    get '/' => "homes#top"
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, except: [:destroy]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
 end

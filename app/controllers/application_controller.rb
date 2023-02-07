f5aedd31578dd3e1e394dc42920c411df94bceb9
Jihye19249205 <119666066+Jihye19249205@users.noreply.github.com>
2023-02-07 13:21:07 +0000

    [update]nagano_cake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  def after_sign_up_path_for(resource)
+    customer_path
+  end
+  
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_path
+    when Customer
+      root_path
+    end
+  end
+  
+  def after_sign_out_path_for(resource)
+    case resource
+    when Admin
+      new_admin_session_path
+    when Customer
+      new_customer_session_path
+    end
+  end
+  
 end
